5349745f992898cced28c87964882c319c6bc9b9
Jianglai <lingxg2024@lzu.edu.cn>
2026-09-15 13:24:19 +0800

    fix: 砍树（二分答案） -- 注意边界值

---
+++ 暑期学校/砍树（二分答案）.c
@@ -0,0 +1,51 @@
+//
+// Created by Jianglai on 2026/9/15.
+//
+
+#include <stdio.h>
+#include<stdio.h>
+#define MAXN 1000005
+
+int heights[MAXN];
+
+int main()
+{
+    int N;   // int 够用
+    long long M;    // 需要的木材总量用long long
+    scanf("%d %lld", &N, &M);
+
+    int maxH = 0;
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &heights[i]);
+        if (heights[i] > maxH)
+        {
+            maxH = heights[i];
+        }
+    }
+
+    int lo = 0;
+    int hi = maxH;
+    while (lo < hi)
+    {
+        int mid = (lo + hi + 1) / 2;
+        long long total = 0;
+        for (int i = 0; i < N;i++)
+        {
+            if (heights[i] > mid)
+            {
+                total += heights[i] - mid;
+            }
+        }
+        if (total >= M)
+        {
+            lo = mid;
+        } else
+        {
+            hi = mid - 1;
+        }
+    }
+
+    printf("%d\n", lo);
+    return 0;
+}
