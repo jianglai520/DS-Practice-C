e8b477bc72ffb8a2ab0b4878c0f992e711c7ccd5
Jianglai <lingxg2024@lzu.edu.cn>
2026-09-15 18:49:01 +0800

    fix: update 卡牌游戏（位运算 + 模拟）

--- 暑期学校/卡牌游戏（位运算 + 模拟）.c
+++ 暑期学校/卡牌游戏（位运算 + 模拟）.c
@@ -4,62 +4,53 @@
 
 #include<stdio.h>
 
-int start[1000005];
-int cnt[1000005];
-int ca[200005];
-int cb[200005];
-char chosen[1000005];
+unsigned long long mask[1000001];
 
 int main()
 {
     int n, t;
-    scanf("%d%d", &n, &t);
-
-    int pos = 0;
+    scanf("%d %d", &n, &t);
 
     for (int i = 1; i <= n; i++)
     {
         int m;
         scanf("%d", &m);
 
-        start[i] = pos;
-        cnt[i] = m;
-
+        unsigned long long cur = 0;
         for (int j = 0; j < m; j++)
         {
-            scanf("%d%d", &ca[pos], &cb[pos]);
-            pos++;
+            int a;
+            unsigned int b;
+            scanf("%d %u", &a, &b);
+
+            if (b & 1U)
+            {
+                cur ^= (1ULL << (a - 1));
+            }
         }
+        mask[i] = cur;
     }
 
     int k;
     scanf("%d", &k);
+
+    unsigned long long ans = 0;
     for (int i = 0; i < k; i++)
     {
-        int p;
-        scanf("%d", &p);
-        chosen[p] = 1;
-    }
-
-    int ans[65] = {0};
-
-    for (int i = 1; i <= n; i++)
-    {
-        if (!chosen[i]) continue;
-
-        for (int j = 0; j < cnt[i]; j++)
-        {
-            int idx = start[i] + j;
-            int card = ca[idx];
-            int num = cb[idx];
-
-            ans[card] ^= (num & 1);
-        }
+        int x;
+        scanf("%d", &x);
+        ans ^= mask[x];
     }
 
     for (int i = 1; i <= t; i++)
     {
-        printf("%d\n", ans[i]);
+        if (ans & (1ULL << (i - 1)))
+        {
+            printf("1\n");
+        } else
+        {
+            printf("0\n");
+        }
     }
 
     return 0;
